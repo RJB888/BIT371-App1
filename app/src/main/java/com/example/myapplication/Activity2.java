8920995814a0d15df5ed2c42f7462be5be531f12
rjb888 <robert.j.bronson@gmail.com>
2020-10-12 10:55:37 -0700

    Password assignment

--- app/src/main/java/com/example/myapplication/Activity2.java
+++ app/src/main/java/com/example/myapplication/Activity2.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
@@ -12,7 +13,13 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
         TextView tv = findViewById(R.id.textView2);
-        String str = getIntent().getStringExtra("msg");
-        tv.setText(str);
+        String name = getIntent().getStringExtra("name").toString();
+        String pass = getIntent().getStringExtra("passWord").toString();
+        Log.i("INFO", "Name: " + name);
+        Log.i("INFO", "Pass: " + pass);
+        if (name.equals("Robert") && pass.equals("1234"))
+            tv.setText("Welcome " + name);
+        else
+            tv.setText("Username / Password combination is wrong");
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void goToActivity2(View view){
-        TextView text = (TextView) findViewById(R.id.textView);
+        EditText uName = findViewById(R.id.userName);
+        EditText passWord = findViewById(R.id.password);
         Intent myIntent = new Intent(getApplicationContext(), Activity2.class);
-        myIntent.putExtra("msg", "Hello New Activity!");
+        myIntent.putExtra("name", uName.getText().toString());
+        myIntent.putExtra("passWord", passWord.getText().toString());
         startActivity(myIntent);
         Log.i("INFO", "switching to activity 2");
     }
